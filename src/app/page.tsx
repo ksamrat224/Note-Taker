c80e29563fb94c99a27f125587729d9078b66516
Samrat <ksamrat224@gmail.com>
2025-10-01 18:57:56 +0545

    text formatter and tailwind css sorting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
-import React from 'react'
+import React from "react";
 
 const HomePage = () => {
-  return (
-    <div className="bg-red-400 h-20">HomePage</div>
-  )
-}
-export default HomePage
+  return <div className="flex h-20 bg-red-400">HomePage</div>;
+};
+export default HomePage;
