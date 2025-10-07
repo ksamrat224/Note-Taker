78ae69ab22d61fefa433e426343d60aa7dc89158
Samrat <ksamrat224@gmail.com>
2025-10-07 10:34:56 +0545

    building homepage

---
+++ src/components/AskAIButton.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { User } from "@supabase/supabase-js";
+import React from "react";
+
+type Props = {
+  user: User;
+};
+export const AskAIButton = ({ user }: Props) => {
+    console.log(user.email);
+  return <div>AskAIButton</div>;
+};
---
+++ src/components/NewNoteButton.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const NewNoteButton = () => {
+  return (
+    <div>NewNoteButton</div>
+  )
+}
