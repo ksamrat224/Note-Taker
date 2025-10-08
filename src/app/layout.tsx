fc3f23ebfd3d8566a3f43db3d1921d0858add57a
Samrat <ksamrat224@gmail.com>
2025-10-08 09:51:47 +0545

    layout covered with noteprovider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Toaster } from "@/components/ui/toaster";
 import Header from "@/components/Header";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import AppSidebar from "@/components/AppSidebar";
+import NoteProvider from "@/providers/NoteProvider";
 
 export const metadata: Metadata = {
   title: "Note Taking App",
@@ -25,19 +26,19 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-
-          <SidebarProvider>
-            <AppSidebar/>
-          <div className="flex min-h-screen w-full flex-col">
-            <Header />
-            <main className="flex flex-1 flex-col px-4 pt-10 xl:px-8">
-              <SidebarTrigger/>
-              {children}
-            </main>
-          </div>
-
-          </SidebarProvider>
-          <Toaster />
+          <NoteProvider>
+            <SidebarProvider>
+              <AppSidebar />
+              <div className="flex min-h-screen w-full flex-col">
+                <Header />
+                <main className="flex flex-1 flex-col px-4 pt-10 xl:px-8">
+                  <SidebarTrigger />
+                  {children}
+                </main>
+              </div>
+            </SidebarProvider>
+            <Toaster />
+          </NoteProvider>
         </ThemeProvider>
       </body>
     </html>
