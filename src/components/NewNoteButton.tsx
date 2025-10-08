f923a88e56b70a40176808f6c4a4251334884db6
Samrat <ksamrat224@gmail.com>
2025-10-08 10:30:30 +0545

    uuid package added and button polished

--- src/components/NewNoteButton.tsx
+++ src/components/NewNoteButton.tsx
@@ -1,11 +1,42 @@
 "use client";
 import { User } from "@supabase/supabase-js";
-import React from "react";
-
+import React, { useState } from "react";
+import { Button } from "./ui/button";
+import { Loader } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { v4 as uuidv4 } from "uuid";
+import { useToast } from "@/hooks/use-toast";
 type Props = {
   user: User | null;
 };
 export const NewNoteButton = ({ user }: Props) => {
+  const router = useRouter();
+  const { toast } = useToast();
+  const [loading, setLoading] = useState(false);
+
+  const handleClickNewNoteButton = async () => {
+    if (!user) {
+      router.push("/login");
+    } else {
+      setLoading(true);
+
+      const uuid = uuidv4();
+      await createNoteAction(uuid);
+      router.push(`/?noteId=${uuid}`);
+
+      setLoading(false);
+    }
+  };
+
   console.log(user?.email);
-  return <div>NewNoteButton</div>;
+  return (
+    <Button
+      onChange={handleClickNewNoteButton}
+      variant="secondary"
+      className="w-24"
+      disabled={loading}
+    >
+      {loading ? <Loader className="animate-spin" /> : "New Note"}
+    </Button>
+  );
 };
