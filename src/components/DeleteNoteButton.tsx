e3270cafc81047477eaba546388de5cadd7e14d3
Samrat <ksamrat224@gmail.com>
2025-10-10 10:05:46 +0545

    delete a particular note

--- src/components/DeleteNoteButton.tsx
+++ src/components/DeleteNoteButton.tsx
@@ -1,12 +1,84 @@
-"use client"
-import React from 'react'
+"use client";
+import React, { useTransition } from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./ui/alert-dialog";
+import { Button } from "./ui/button";
+import { Loader2, Trash2 } from "lucide-react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useToast } from "@/hooks/use-toast";
+import { deleteNoteAction } from "@/actions/notes";
 
-type Props={
-    noteId:string,
-    deleteNoteLocally:(noteId:string)=>void;
-}
-export const DeleteNoteButton = ({noteId,deleteNoteLocally}:Props) => {
+type Props = {
+  noteId: string;
+  deleteNoteLocally: (noteId: string) => void;
+};
+export const DeleteNoteButton = ({ noteId, deleteNoteLocally }: Props) => {
+  const router = useRouter();
+  const { toast } = useToast();
+  const noteIdParam = useSearchParams().get("noteId") || "";
+  const [isPending, startTransition] = useTransition();
+  const handleDeleteNote = () => {
+    startTransition(async () => {
+      const { errorMessage } = await deleteNoteAction(noteId);
+
+      if (!errorMessage) {
+        toast({
+          title: "Note deleted",
+          description: "Your note has been deleted successfully",
+          variant: "success",
+        });
+        deleteNoteLocally(noteId);
+        if (noteId === noteIdParam) {
+          router.replace(`/`);
+        }
+      } else {
+        toast({
+          title: "Error",
+          description: errorMessage,
+          variant: "destructive",
+        });
+      }
+    });
+  };
   return (
-    <div>DeleteNoteButton</div>
-  )
-}
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button
+          className="absolute top-1/2 right-2 size-7 -translate-y-1/2 opacity-0 transition-opacity group-hover/item:opacity-100 [&_svg]:size-3"
+          variant={"ghost"}
+        >
+          <Trash2 />
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            Are you you want to delete this note?
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            note.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={handleDeleteNote}
+            className="bg-destructive text-destructive-foreground hover:bg-destructive/90 w-24"
+          >
+            {isPending ? <Loader2 className="animate-spin" /> : "Delete"}
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
