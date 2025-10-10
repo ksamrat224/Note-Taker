37d8a7a0f907b497c0a00d446dc62915f8a5b18e
Samrat <ksamrat224@gmail.com>
2025-10-10 07:26:38 +0545

    searchbar added in sidebar

--- src/components/SidebarGroupContent.tsx
+++ src/components/SidebarGroupContent.tsx
@@ -1,12 +1,26 @@
 "use client";
 import { Note } from "@prisma/client";
-import React from "react";
+import React, { useState } from "react";
 import { SidebarGroupContent as SidebarGroupContentShadCN } from "./ui/sidebar";
+import { SearchIcon } from "lucide-react";
+import { Input } from "./ui/input";
 type Props = {
   notes: Note[];
 };
 
 export const SidebarGroupContent = ({ notes }: Props) => {
-  console.log(notes);
-  return <SidebarGroupContentShadCN>Your notes here</SidebarGroupContentShadCN>;
+  const [searchText, setSearchText] = useState("");
+  return (
+    <SidebarGroupContentShadCN>
+      <div className="relative flex items-center">
+        <SearchIcon className="absolute left-2 size-2" />
+        <Input
+          className="bg-muted pl-8"
+          placeholder="search your notes"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+      </div>
+    </SidebarGroupContentShadCN>
+  );
 };
