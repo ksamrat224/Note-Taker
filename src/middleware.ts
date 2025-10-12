387724c948aef4d09341daae6f6fb2435c8413e7
Samrat <ksamrat224@gmail.com>
2025-10-12 08:31:32 +0545

    middle ware route

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,32 +15,47 @@ export async function updateSession(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
     request,
   });
-  console.log("middleware runninng");
 
-  //   const supabase = createServerClient(
-  //     process.env.SUPABASE_URL!,
-  //     process.env.SUPABASE_PUBLISHABLE_KEY!,
-  //     {
-  //       cookies: {
-  //         getAll() {
-  //           return request.cookies.getAll()
-  //         },
-  //         setAll(cookiesToSet) {
-  //           cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value))
-  //           supabaseResponse = NextResponse.next({
-  //             request,
-  //           })
-  //           cookiesToSet.forEach(({ name, value, options }) =>
-  //             supabaseResponse.cookies.set(name, value, options)
-  //           )
-  //         },
-  //       },
-  //     }
-  //   )
+  const supabase = createServerClient(
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value }) =>
+            request.cookies.set(name, value),
+          );
+          supabaseResponse = NextResponse.next({
+            request,
+          });
+          cookiesToSet.forEach(({ name, value, options }) =>
+            supabaseResponse.cookies.set(name, value, options),
+          );
+        },
+      },
+    },
+  )
 
-  //   const {
-  //     data: { user },
-  //   } = await supabase.auth.getUser()
+  const isAuthRoute =
+    request.nextUrl.pathname === "/login" ||
+    request.nextUrl.pathname === "/sign-up";
+  if (isAuthRoute) {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+    if (user) {
+      return NextResponse.redirect(
+        new URL("/", process.env.NEXT_PUBLIC_BASE_URL!),
+      );
+    }
+  }
+
+  // const {
+  //   data: { user },
+  // } = await supabase.auth.getUser();
 
   return supabaseResponse;
 }
